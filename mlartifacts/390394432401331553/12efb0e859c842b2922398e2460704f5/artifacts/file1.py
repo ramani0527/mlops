fe5779b089ede4e2c05160225c6e68bdad36d9e0
Ramani <ramani0801@outlook.com>
2026-03-18 10:34:12 -0700

    Add MLflow integration for Random Forest model training and evaluation
    - Implemented a script to train a Random Forest Classifier on the wine dataset.
    - Configured MLflow tracking URI and set up experiment logging.
    - Logged model parameters, metrics, and artifacts including a confusion matrix plot.
    - Added tags for model versioning and user identification.
    - Created a new experiment in MLflow for organized tracking of model runs.
    - Included necessary imports and visualizations for model performance evaluation.

---
+++ mlartifacts/390394432401331553/12efb0e859c842b2922398e2460704f5/artifacts/file1.py
@@ -0,0 +1,54 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+"""To rectify the error - The configured tracking 
+uri scheme: 'file' is invalid for use with the proxy mlflow-artifact scheme. The allowed tracking schemes are: {'http', 'https'}
+we are writing this line below"""
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+#Load wind dataset
+wine= load_wine()
+X= wine.data
+y= wine.target
+#train and test split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.10, random_state = 42)
+
+# Define the params for ml model
+max_d = 5
+n_esti = 8
+#Mention your experiment below
+mlflow.set_experiment("YT-MLOPS-exp-1")
+# First run 
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_d, n_estimators=n_esti, random_state = 42)
+    rf.fit(X_train,y_train)
+    
+    y_pred = rf.predict(X_test)
+    accuracy= accuracy_score(y_pred,y_test)
+    
+    mlflow.log_param("max_depth",max_d)
+    mlflow.log_param("n_estimators",n_esti)
+    mlflow.log_metric('accuracy', accuracy)
+    
+
+
+    #Creating Confusion Matrix plot as second run 
+    
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel("Actual")
+    plt.xlabel("Predicted")
+    plt.title('Confusion Matrix')
+    #save.plot
+    plt.savefig("Wine_Confusion_Matrix.png")
+    
+    #log actifacts using mlflow
+    mlflow.log_artifact("Wine_Confusion_Matrix.png") 
+    mlflow.log_artifact(__file__)
+    
+    print('accuracy',accuracy)
